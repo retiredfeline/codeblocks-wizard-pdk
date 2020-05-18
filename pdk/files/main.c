5a522c56e38054211313016e6b599535814d3ec1
Ken Yap <kenyap.com.au@outlook.com>
2020-05-18 23:50:27 +1000

    Added call to tuning code and hint to define model

--- pdk/files/main.c
+++ pdk/files/main.c
@@ -1,12 +1,22 @@
 /*
- */
+**      Sample main program
+**      Remember to add -D<processor_model> to compiler arguments
+*/
+
+#include "easypdk/pdk.h"
+
+unsigned char _sdcc_external_startup(void)
+{
+    // example, edit to suit
+    EASY_PDK_INIT_SYSCLOCK_8MHZ();            //use 8MHz sysclock
+    EASY_PDK_CALIBRATE_IHRC(8000000,4000);    //tune SYSCLK to 8MHz @ 4.000V
+    return 0;                                 //perform normal initialization
+}
 
 void main(void)
 {
-
     // Insert code
 
     while(1)
         ;
-
 }
